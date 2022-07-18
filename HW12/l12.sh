25cb36556afbf0acf925c1e138dbfce41becb76b
STAS <jest96@gmail.com>
2022-07-18 18:35:38 +0300

    script fix

--- HW12/l12.sh
+++ HW12/l12.sh
@@ -3,11 +3,10 @@
 if [ ! -d /home/stas/examples/lesson12 ]; then
   mkdir -p /home/stas/examples/lesson12;
 fi
-TODAY=$(date +"Today is %A, %d of %B")
-TIMENOW=$(date+"The local time is %r")
-echo "1 - $TODAY $TIMENOW" >/home/stas/examples/lesson12/examplelesson12.txt
-echo '2 - $TODAY $TIMENOW' >> /home/stas/examples/lesson12/examplelesson12.txt
-echo `3 - $TODAY $TIMENOW` >> /home/stas/examples/lesson12/examplelesson12.txt
+TODAY=$(date )
+echo "$TODAY" >/home/stas/examples/lesson12/examplelesson12.txt
+echo '$TODAY' >> /home/stas/examples/lesson12/examplelesson12.txt
+echo `$TODAY` >> /home/stas/examples/lesson12/examplelesson12.txt
 
 # as a string
 a=879
