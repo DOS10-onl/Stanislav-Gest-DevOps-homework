ca25ef24cd000d7ac7fc62fe1a7d6ff88158a083
STAS <jest96@gmail.com>
2022-08-08 19:25:14 +0300

    solving l16 git conflicts

--- HW12/l12.sh
+++ HW12/l12.sh
@@ -3,10 +3,12 @@
 if [ ! -d /home/stas/examples/lesson12 ]; then
   mkdir -p /home/stas/examples/lesson12;
 fi
-TODAY=$(date )
-echo "$TODAY" >/home/stas/examples/lesson12/examplelesson12.txt
-echo '$TODAY' >> /home/stas/examples/lesson12/examplelesson12.txt
-echo `$TODAY` >> /home/stas/examples/lesson12/examplelesson12.txt
+TODAY=$(date +"Today is %A, %d of %B")
+TIMENOW=$(date+"The local time is %r")
+echo "1 - $TODAY $TIMENOW" >/home/stas/examples/lesson12/examplelesson12.txt
+echo '2 - $TODAY $TIMENOW' >> /home/stas/examples/lesson12/examplelesson12.txt
+echo `3 - $TODAY $TIMENOW` >> /home/stas/examples/lesson12/examplelesson12.txt
+
 
 # as a string
 a=879
@@ -21,4 +23,6 @@ read c
 echo -n "Input \"d\" " 
 read d 
 e=$(($c+$d)) 
+
 echo "Значение переменной \"e = c+d\" теперь стало равным: $e">>/home/stas/examples/lesson12/examplelesson12.txt
+
